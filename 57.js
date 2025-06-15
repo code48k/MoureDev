14ba546f04793b95a6fef22c60c9f05a7c03faf5
*** Lucas <jmlucasdelamo@gmail.com>
2025-06-15 02:19:24 +0200

    borrado

--- 57.js
+++ 57.js
@@ -1,1 +1,9 @@
 // 57.- Escribe un bucle que imprima la tabla de multiplicar del 5. Usa diferentes bucles tanto while, do while y for!.
+
+for (let buclfr = 0; buclfr<10; buclfr++){
+
+    tablaMultiplicar = buclfr;
+
+}
+
+console.log(tablaMultiplicar);
