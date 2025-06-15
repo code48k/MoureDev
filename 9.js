49824ce44ed5f9207dca1b4a119439924256ddef
*** Lucas <jmlucasdelamo@gmail.com>
2025-06-15 15:39:22 +0200

    A continuación, modifica los valores de las constantes.

--- 9.js
+++ 9.js
@@ -1,3 +1,3 @@
 // 9.- A continuación, modifica los valores de las constantes.
 
-// No se pueden modificar las variables constantes.
+// No se pueden modificar las variables constantes, son datos inmutables.
