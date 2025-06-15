e1e3c821ef6ffdc7c1ddf676db906be6b7c15fdd
*** Lucas <jmlucasdelamo@gmail.com>
2025-06-15 16:23:10 +0200

    Imprime 5 vomparaciones falsas com diferentes operadores de compraración.

--- 14.js
+++ 14.js
@@ -3,8 +3,8 @@
 const a = 5;
 const b = 15;
 
-console.log(a > b);
-console.log(a !== a);
-console.log(!b == 15);
-console.log(1 == false);
-console.log(undefined === null);
+console.log(a > b); // Mayor
+console.log(a !== a); // Estrictamente desigual
+console.log(!b == 15); // Distinto igual
+console.log(1 == false); // Igualdad
+console.log(undefined === null); // Esctritamente igual
