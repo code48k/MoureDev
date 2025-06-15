7181e00968985afa08d3c135a1daf6427e678067
*** Lucas <jmlucasdelamo@gmail.com>
2025-06-15 15:32:46 +0200

    Declara variables con valores asociados a todos los datos de tipo primitivo.

--- 3.js
+++ 3.js
@@ -10,20 +10,25 @@ const numero = 24;
 const decimal = 1.72;
 
 // Booleano
+
 const verdadero = true;
 const falso = false;
 
 // El valor null es un literal de Javascript que representa intencionalmente un valor nulo o "vacío".
 // Null
+
 let nulo = null;
 
 // Una variable a la que no se le ha asignado valor es de tipo undefined. Un método o sentencia también devuelve undefined si la variable que se está evaluando no tiene asignado un valor.
 // Undefined
+
 let undefined;
 
 // Symbol
+
 let symbol = Symbol("mySimbol");
 
 //BigInt (Número Grandes)
+
 const numeroGrandes = BigInt(39889389338389339889339789393893899389383983383838993);
 const numeroGrande = 39889389338389339889339789393893899389383983383838993n;
