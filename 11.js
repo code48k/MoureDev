c124354678c18f305d2124bbceb4bb26c322cbf9
*** Lucas <jmlucasdelamo@gmail.com>
2025-06-15 15:46:45 +0200

    Crea una variable para cada operación aritmética.

--- 11.js
+++ 11.js
@@ -3,10 +3,10 @@
 let a = 5;
 let b = 10;
 
-console.log(a + b);
-console.log(a - b);
-console.log(a * b);
-console.log(a / b);
+console.log(a + b); // Suma
+console.log(a - b); // Resta
+console.log(a * b); // Multiplicación
+console.log(a / b); // División
 
-console.log(a % b);
-console.log(a ** b);
+console.log(a % b); // Módulo
+console.log(a ** b); // Exponente
