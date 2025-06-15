565b638e505e4066dd27aeaae828bb8128e8e68f
*** Lucas <jmlucasdelamo@gmail.com>
2025-06-15 15:50:13 +0200

    Imprime 5 comparaciones con verdades con diferentes operaciones de comparación.

--- 13.js
+++ 13.js
@@ -3,8 +3,8 @@
 const a = 5;
 const b = 15;
 
-console.log(a < b);
-console.log(a == a);
-console.log(b === 15);
-console.log(b == "15");
-console.log(a >=5);
+console.log(a < b); // Menor
+console.log(a == a); // Igualdad
+console.log(b === 15); // Estrictamente igual
+console.log(b == "15"); // igual de distinto tipo
+console.log(a >=5); // Mayor igual
