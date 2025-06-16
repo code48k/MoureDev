98a2baaa606757c280412ce9358d669d3637f826
*** Lucas <jmlucasdelamo@gmail.com>
2025-06-16 21:32:04 +0200

    Comprueba si dos strings son iguales.

--- 29.js
+++ 29.js
@@ -1,4 +1,4 @@
-// 29.- Comprueba si dos strings son iguales.
+// 29.- Comprueba si dos strings son iguales. 
 
 let cadenaTrue1 = "Estoy encantado de la Vida de JavaScript";
 let cadenaTrue2 = "Estoy encantado de la Vida de JavaScript";
