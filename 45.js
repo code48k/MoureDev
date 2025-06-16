74a16f1dab5a7f1eab49ce6b9096add75b8dae8c
*** Lucas <jmlucasdelamo@gmail.com>
2025-06-17 01:42:31 +0200

    Crea un set que almacene cinco libros y añade dos más. Uno de ellos repetido.

--- 45.js
+++ 45.js
@@ -1,4 +1,4 @@
-// 45.- Crea un set que almacene cinco libros y añade dos más. Uno de ellos repetido.
+// 45.- Crea un set que almacene cinco libros y añade dos más. Uno de ellos repetido. 
 
 let libros = new Set();
 libros = new Set(["La Garganta Profunda", "Harry Potter", "Adiccion a la Vida", "La Casa Negra", "Pepito de los Palotes"]);
