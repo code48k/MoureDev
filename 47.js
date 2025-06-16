f0718d5cb893763a28d1123cd113c485b0c8c4ff
*** Lucas <jmlucasdelamo@gmail.com>
2025-06-17 01:44:33 +0200

    Crea un mapa que asocie el número del mes a su nombre.

--- 47.js
+++ 47.js
@@ -1,7 +1,7 @@
-// 47.- Crea un mapa que asocie el número del mes a su nombre.
+// 47.- Crea un mapa que asocie el número del mes a su nombre. 
 
 let maps = new Map([
     ["Diciembre", "Fecha 12"],
     ["Enero", "Fecha 01"]
 ])
-console.log(maps);
+console.log(maps); 
