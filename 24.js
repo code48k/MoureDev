4c53f65a28ff4882767604bee01575b4c1543ab6
*** Lucas <jmlucasdelamo@gmail.com>
2025-06-16 21:26:25 +0200

    Convierte a mayúscula y minúsculas un string.

--- 24.js
+++ 24.js
@@ -1,5 +1,5 @@
 // 24.- Convierte a mayúscula y minúsculas un string.
 
 let saludo = "¡Hola Mundo!";
-console.log(saludo.toUpperCase());
+console.log(saludo.toUpperCase()); 
 console.log(saludo.toLocaleLowerCase());
