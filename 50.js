8eb5ac9c82e62e1b7fdcf4c2b023e05f4728ef2f
*** Lucas <jmlucasdelamo@gmail.com>
2025-06-18 01:40:28 +0200

    Crea un array, transfórmalo a un set y almacénalo en un map.

--- 50.js
+++ 50.js
@@ -1,1 +1,12 @@
 // 50.- Crea un array, transfórmalo a un set y almacénalo en un map.
+
+const miArray = [1, 2, 2, 3, 4, 4, 5];
+
+const miSet = new Set(miArray);
+
+const miMap = new Map();
+miMap.set('miConjunto', miSet);
+
+console.log("Array original:", miArray);
+console.log("Set creado:", miSet);
+console.log("Map con el Set:", miMap);
