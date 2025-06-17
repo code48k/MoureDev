26dc6aa454b10775f32613c2b9a4f53a2f614c37
*** Lucas <jmlucasdelamo@gmail.com>
2025-06-18 01:31:47 +0200

    Usa un bucle para invertir una cadena de texto. Usa diferentes bucles tanto while, do while y for!.

--- 58.js
+++ 58.js
@@ -1,3 +1,47 @@
 // 58.- Usa un bucle para invertir una cadena de texto. Usa diferentes bucles tanto while, do while y for!.   
 
- 
+let cadena = "Hola, soy Javascript, encantado de conocerte amigo!!!";
+
+let inversionCadena = "";
+
+for (let buclfr = cadena.length-1; buclfr>=0; buclfr--){
+
+    inversionCadena += cadena[buclfr];
+
+}
+
+console.log(inversionCadena);
+
+// --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+let bucWhle = cadena.length-1;
+
+inversionCadena = "";
+
+while (bucWhle>=0){
+
+    inversionCadena += cadena[bucWhle];
+
+    bucWhle--;
+
+}
+
+console.log(inversionCadena);
+
+// --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+let bucDoWhle = cadena.length-1;
+
+inversionCadena = "";
+
+do {
+
+    inversionCadena += cadena[bucDoWhle];
+
+    bucDoWhle--;
+
+}
+
+while (bucDoWhle>=0);
+
+console.log(inversionCadena);
