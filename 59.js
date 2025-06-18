01531697f8530cddb20f331eb2897278b5096c68
*** Lucas <jmlucasdelamo@gmail.com>
2025-06-18 02:20:02 +0200

    estoy aqui

--- 59.js
+++ 59.js
@@ -1,1 +1,35 @@
 // 59.- Usa un bucle para generar los primeros 10 números de la secuencia de Fibonacci. Usa diferentes bucles tanto while, do while y for!. 
+
+
+// La sucesión de Fibonacci es una secuencia infinita de números donde cada número se obtiene sumando los dos números anteriores. La secuencia comienza con 0 y 1, y se ve así: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, y así sucesivamente. Esta secuencia fue descrita por primera vez por Leonardo de Pisa, también conocido como Fibonacci, en su libro "Liber Abaci" en 1202. 
+// La fórmula para generar la secuencia de Fibonacci es: F(n) = F(n-1) + F(n-2), donde F(0) = 0 y F(1) = 1. 
+
+
+let numero = "0";
+
+let inicio = "1";
+
+for (let i = 0; i<inicio.length; i++){
+    resultado = numero + inicio;
+
+    //  numero = fibonacci[i-1] + fibonacci[fibonacci.length-1];
+
+
+}
+
+console.log(resultado);
+
+
+// let fibonacci = [1,1];
+// while (fibonacci.length < 20) {
+//   fibonacci.push( fibonacci[fibonacci.length -1] + fibonacci[fibonacci.length -2]);
+// }
+
+// console.log(fibonacci.toString());
+
+
+// buclfr
+
+// bucWhle
+
+// bucDoWhle
