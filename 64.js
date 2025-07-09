1383f9782b5860b31566880f99ec26b040199a55
*** Lucas <jmlucasdelamo@gmail.com>
2025-07-10 00:41:10 +0200

    Crea una función que reciba un array de strings y devuelva un nuevo array con las strings en mayúsculas. Explora diferentes sintaxis de funciones para resolver el ejercicio.

--- 64.js
+++ 64.js
@@ -1,1 +1,72 @@
 // 64.- Crea una función que reciba un array de strings y devuelva un nuevo array con las strings en mayúsculas. Explora diferentes sintaxis de funciones para resolver el ejercicio.
+
+let cadena = ["¡Hola, soy Javascript"];
+
+function arrayStringSimple(){
+
+    for (let i=0; i<cadena.length; i++){
+
+        let frase = cadena[i].toUpperCase();
+        let resultado = [frase];
+        console.log(resultado);
+
+    }   
+
+}
+
+arrayStringSimple();
+
+// --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// Función parametros
+
+function arrayStringParameters(str){
+    
+    for (let i =0; i<str.length; i++){
+
+        let resultado = str[i].toUpperCase();
+        console.log([resultado]);
+    }
+}
+
+arrayStringParameters(["¡He cambiado a PHP lenguaje de servidor!!!"]);
+
+// --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// Funcion Anónima
+
+cadena = ["¡Ahora soy Java!!!..."];
+
+const arrayStringAnonymous = function(){
+
+    for (let i=0; i<cadena.length; i++){
+
+        let resultado = cadena[i].toUpperCase();
+        console.log([resultado]);
+    }
+
+}
+
+arrayStringAnonymous();
+
+// --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// Funciones Flecha
+
+cadena = ["¡Me convertido en el famoso .NET!!!..."];
+
+const resultArrow = () => {
+
+    for (let i =0; i<cadena.length; i++){
+
+        let resultado = cadena[i].toUpperCase();
+        console.log([resultado]);
+    }
+
+}
+
+resultArrow();
+
+// --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// Retorno de Valores
