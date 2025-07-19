32a637ca63d2e012cecf4b4d4212d0b07312282a
*** Lucas <jmlucasdelamo@gmail.com>
2025-07-19 10:56:16 +0200

    Crea una función que reciba una cadena de texto y devuelva la misma cadena con las palabras en orden inverso. Explora diferentes sintaxis de funciones para resolver el ejercicio.

--- 69.js
+++ 69.js
@@ -1,1 +1,102 @@
 // 69.- Crea una función que reciba una cadena de texto y devuelva la misma cadena con las palabras en orden inverso. Explora diferentes sintaxis de funciones para resolver el ejercicio.
+
+let cadena = "¡Hola, soy JavasCript!!!!!";
+
+let cadenaInvertida = "";
+
+function receiveChainSimple(){
+
+    for (let i = cadena.length; i--;){
+
+        cadenaInvertida += cadena[i];
+
+    }
+}
+
+receiveChainSimple();
+
+console.log(cadenaInvertida);
+
+// --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// Función parametros
+
+cadenaInvertida = "";
+
+function receiveArrayParameters(str){
+
+    for (let i=str.length; i--;){
+
+        cadenaInvertida += str[i];
+
+    }
+
+}
+
+receiveArrayParameters("¡Hola, soy JavasCript!!!!!");
+
+console.log(cadenaInvertida);
+
+// --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// Funcion Anónima
+
+cadena = "¡Hola, soy JavasCript!!!!!";
+
+cadenaInvertida = "";
+
+const resultArrayAnonymous = function(){
+
+    for (let i = cadena.length; i--;){
+
+        cadenaInvertida += cadena[i];
+
+    }
+}
+
+resultArrayAnonymous();
+
+console.log(cadenaInvertida);
+
+// --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// Funciones Flecha
+
+cadena = "¡Hola, soy JavasCript!!!!!";
+
+cadenaInvertida = "";
+
+const resultArrayArrow = () =>{
+
+    for (let i= cadena.length; i--;){
+
+        cadenaInvertida += cadena[i];
+        
+    }
+
+}
+
+resultArrayArrow();
+
+console.log(cadenaInvertida);
+
+
+// --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// Retorno de Valores
+
+cadenaInvertida = "";
+
+function resultArrayReturnValues(str){
+
+    for (let i=str.length; i--;){
+
+        cadenaInvertida += str[i];
+
+    }
+
+}
+
+resultArrayReturnValues("¡Hola, soy JavasCript!!!!!");
+
+console.log(cadenaInvertida);
