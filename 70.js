157f8e351732ede6c8d325da24151a79246f19d3
*** Lucas <jmlucasdelamo@gmail.com>
2025-07-20 13:35:11 +0200

    Crea una función que reciba un array de números y devuelva la suma de todos los números pares. Explora diferentes sintaxis de funciones para resolver el ejercicio.

--- 70.js
+++ 70.js
@@ -1,1 +1,5 @@
 // 70.- Crea una función que calcule el factorial de un número dado. Explora diferentes sintaxis de funciones para resolver el ejercicio.
+
+let factorial = 5*4*3*2*1
+
+console.log(factorial);
